4214b341aebd198287d741a5639eb2e55b23289b
Brian Wangila <bwanjalah@gmail.com>
2022-08-18 17:49:47 +0300

    commit tree 621542f7e8680a4ee6fd8b1cadcbbf6bff50a89a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
+  "Hi, my name is Sophie.".sentence?
+
 
   def question?
-
+    self.end_with?("?")
   end
+
 
   def exclamation?
-
+    self.end_with?("!")
   end
+
 
   def count_sentences
-
+    
+    split_sentence = self.split(/[.!?]/)
+      filtered = split_sentence.filter { |word| !word.empty?}
+      filtered.size
   end
 end
+"this is me. is? me!".count_sentences
+
+
+class String
+
+  def report_on_self
+    puts "Self is: #{self}"
+  end
+end
+
+"hello".report_on_self
+"this string".report_on_self
